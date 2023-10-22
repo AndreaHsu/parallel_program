b29a99c9ab8a764103d8f00a44a823214495d2a1
109062101 <andrea.843843@gmail.com>
2023-10-22 23:43:23 +0800

    update version

--- HW/hw1/hw1.cc
+++ HW/hw1/hw1.cc
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 #include <mpi.h>
-#include <boost/sort/spreadsort/spreadsort.hpp>
+#include <boost/sort/spreadsort/float_sort.hpp>
 using namespace std;
-#define EVEN_STAGE 1
-#define ODD_STAGE 2
+#define COMM_STAGE 1
 
 void radix_sort(float* arr, int len){
     for(int i = 0; i < len; i++){
@@ -29,48 +28,6 @@ void radix_sort(float* arr, int len){
     }
 }
 
-void merge_large(int sizeA, int sizeB, int sizeC, float* dataA, float* dataB, float* dataC){
-    int idxA = sizeA - 1;
-    int idxB = sizeB - 1;
-    int idxC = sizeC - 1;
-
-    while(idxA >= 0 && idxB >= 0){// && idxC >= 0){
-        if(dataA[idxA] > dataB[idxB]){
-            dataC[idxC--] = dataA[idxA--];
-        }else{
-            dataC[idxC--] = dataB[idxB--];
-        }
-        if(idxC < 0) return;
-    }
-
-    while(idxC >= 0){
-        dataC[idxC--] = dataA[idxA--];
-    }
-
-    return ;
-}
-
-void merge_small(int sizeA, int sizeB, int sizeC, float* dataA, float* dataB, float* dataC){
-    int idxA = 0;
-    int idxB = 0;
-    int idxC = 0;
-
-    while(idxA < sizeA && idxB < sizeB){// && idxC < sizeC){
-        if(dataA[idxA] < dataB[idxB]){
-            dataC[idxC++] = dataA[idxA++];
-        }else{
-            dataC[idxC++] = dataB[idxB++];
-        }
-        if(idxC >= sizeC) return;
-    }
-
-    while(idxC < sizeC){
-        dataC[idxC++] = dataA[idxA++];
-    }
-
-   return;
-}
-
 void debugPrint(int rank, int n, int s, float* buf, string c){
     cout << "Debug Print MyRank: " << rank  << ", recieved from OtherRank: " << n << ", case: " << c << endl;
     for(int i = 0; i < s; i++){
@@ -82,25 +39,25 @@ void debugPrint(int rank, int n, int s, float* buf, string c){
 int main(int argc, char ** argv){
     MPI_Init(&argc, &argv);
     int rank;
-    int numpOfProcess;
+    int numOfProcess;
     int arrSize = atoi(argv[1]);
     int localSize;
     int localOffset;
     float* localDataBuf;
 
-    MPI_Comm_size(MPI_COMM_WORLD, &numpOfProcess);
+    MPI_Comm_size(MPI_COMM_WORLD, &numOfProcess);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int flag = (rank % 2);
     
-    if(numpOfProcess > arrSize) numpOfProcess = arrSize;
+    if(numOfProcess > arrSize) numOfProcess = arrSize;
     // 1. Partition data
-    int quotient = arrSize / numpOfProcess;
-    int remainder = arrSize % numpOfProcess; 
-    localSize = (rank < remainder) ? (quotient + 1) : (quotient);
+    int quotient = arrSize / numOfProcess;
+    int remainder = arrSize % numOfProcess; 
+    localSize = quotient + (rank < remainder);
     localOffset = (rank < remainder) ? (sizeof(float) * rank * localSize) : (sizeof(float) * (remainder + rank * localSize));
-    localDataBuf = (float*) malloc(sizeof(float) * localSize);
+    localDataBuf = new float[localSize];
 
     if(rank >= arrSize) {
+        localOffset = 0;
         localSize = 0;
         rank = -1;
     }
@@ -110,99 +67,81 @@ int main(int argc, char ** argv){
     MPI_File_open(MPI_COMM_WORLD, argv[2], MPI_MODE_RDONLY, MPI_INFO_NULL, &fin);
     MPI_File_read_at(fin, localOffset, localDataBuf, localSize, MPI_FLOAT, MPI_STATUS_IGNORE);
     MPI_File_close(&fin);
-
+    
     // if(localSize) radix_sort(localDataBuf, localSize);
-    if(localSize) boost::sort::spreadsort::spreadsort(localDataBuf, localDataBuf+localSize);
+    if(localSize) boost::sort::spreadsort::float_sort(localDataBuf, localDataBuf+localSize);
 
-    // 3. Send and Recv data between neighbor node
-    // 3-1. find neighbor and the localSize of neighbor
-    int evenNeighbor; 
-    int oddNeighbor;
-    int evenSize;
-    int oddSize;
-    float* evenRecvBuf;
-    float* oddRecvBuf;
-
-    if(flag){
-        // rank is odd
-        evenNeighbor = rank - 1;
-        oddNeighbor = (rank == (numpOfProcess - 1)) ? MPI_PROC_NULL : (rank + 1);
-    }else{
-        // rank is even
-        evenNeighbor = (rank == (numpOfProcess - 1)) ? MPI_PROC_NULL : (rank + 1);
-        oddNeighbor = (rank == 0) ? MPI_PROC_NULL : (rank - 1);
-    }
-
-    evenSize = (evenNeighbor < remainder) ? (quotient + 1) : (quotient);
-    oddSize = (oddNeighbor < remainder) ? (quotient + 1) : (quotient);
-
-    evenRecvBuf = (float*) malloc(sizeof(float) * evenSize);
-    oddRecvBuf = (float*) malloc(sizeof(float) * oddSize);
-    // 3-2. SendRecv
+    // 3. Send and Recv data between neighbor node: find neighbor and the localSize of neighbor
+    int leftNeighbor = rank - 1; 
+    int rightNeighbor = (rank == (numOfProcess - 1)) ? MPI_PROC_NULL : (rank + 1);
+    int leftSize = quotient + (leftNeighbor < remainder); 
+    int rightSize = quotient + (rightNeighbor < remainder); 
+    float* recvDataBuf = new float[leftSize];
     
     // 4. Odd Even Sort
-    float* mergeDataBuf = (float*) malloc(sizeof(float) * localSize);
-    int maxSortNum = numpOfProcess+1;
-    int checkIdx;
-    MPI_Request sendReq1, recvReq1, sendReq2, recvReq2;
-    
-    for(int i = 0; i < maxSortNum; i++){
-        if(rank == -1) break;
-        if(i % 2){
-            // Odd Stage
-            if(oddNeighbor < 0) continue;
-            // take the head and buttom to decide how many data need to exchange
-            checkIdx = flag ? localSize-1 : 0;
-            MPI_Isend(&localDataBuf[checkIdx], 1, MPI_FLOAT, oddNeighbor, ODD_STAGE, MPI_COMM_WORLD, &sendReq1);
-            MPI_Irecv(oddRecvBuf, 1, MPI_FLOAT, oddNeighbor, ODD_STAGE, MPI_COMM_WORLD, &recvReq1);
-            MPI_Wait(&sendReq1, MPI_STATUS_IGNORE);
-            MPI_Wait(&recvReq1, MPI_STATUS_IGNORE);
-            if(flag){
-                if(oddRecvBuf[0] > localDataBuf[checkIdx]) continue;
-            }else{
-                if(oddRecvBuf[0] < localDataBuf[checkIdx]) continue;
-            }
+    float* mergeDataBuf = new float[localSize];
+    int maxSortNum = numOfProcess+1;
+    bool stage = rank & 1;
+    int idxA, idxB, idxC;
+    MPI_Request sendReq, recvReq, sendReq1, recvReq1, sendReq2, recvReq2;
 
-            MPI_Isend(localDataBuf, localSize, MPI_FLOAT, oddNeighbor, ODD_STAGE, MPI_COMM_WORLD, &sendReq2);
-            MPI_Irecv(oddRecvBuf, oddSize, MPI_FLOAT, oddNeighbor, ODD_STAGE, MPI_COMM_WORLD, &recvReq2);
-            MPI_Wait(&sendReq2, MPI_STATUS_IGNORE);
-            MPI_Wait(&recvReq2, MPI_STATUS_IGNORE);
-            if(flag){
-                // rank is odd, get smaller half
-                merge_small(localSize, oddSize, localSize, localDataBuf, oddRecvBuf, mergeDataBuf);
-            }else{
-                // rank is even, get larger half
-                merge_large(localSize, oddSize, localSize, localDataBuf, oddRecvBuf, mergeDataBuf);
-            }
-        }else{
-            // Even Stage
-            if(evenNeighbor < 0) continue;
+    while(maxSortNum--){
+        if(rank == -1) break;
+        if(stage && rightNeighbor != -1){
+            // get the smaller half
             // take the head and buttom to decide how many data need to exchange
-            checkIdx = flag ? 0 : localSize-1;
-            MPI_Isend(&localDataBuf[checkIdx], 1, MPI_FLOAT, evenNeighbor, EVEN_STAGE, MPI_COMM_WORLD, &sendReq1);
-            MPI_Irecv(evenRecvBuf, 1, MPI_FLOAT, evenNeighbor, EVEN_STAGE, MPI_COMM_WORLD, &recvReq1);
-            MPI_Wait(&sendReq1, MPI_STATUS_IGNORE);
-            MPI_Wait(&recvReq1, MPI_STATUS_IGNORE);
-            if(flag){
-                if(evenRecvBuf[0] < localDataBuf[checkIdx]) continue;
-            }else{
-                if(evenRecvBuf[0] > localDataBuf[checkIdx]) continue;
-            }
-            
-            MPI_Isend(localDataBuf, localSize, MPI_FLOAT, evenNeighbor, EVEN_STAGE, MPI_COMM_WORLD, &sendReq2);
-            MPI_Irecv(evenRecvBuf, evenSize, MPI_FLOAT, evenNeighbor, EVEN_STAGE, MPI_COMM_WORLD, &recvReq2);
-            MPI_Wait(&sendReq2, MPI_STATUS_IGNORE);
-            MPI_Wait(&recvReq2, MPI_STATUS_IGNORE);
-            if(flag){
-                // rank is odd, get larger half
-                merge_large(localSize, evenSize, localSize, localDataBuf, evenRecvBuf, mergeDataBuf);
-            }else{
-                // rank is even, get smaller half
-                merge_small(localSize, evenSize, localSize, localDataBuf, evenRecvBuf, mergeDataBuf);
+            MPI_Sendrecv(&localDataBuf[localSize-1], 1, MPI_FLOAT, rightNeighbor, COMM_STAGE, recvDataBuf, 1, MPI_FLOAT, rightNeighbor, COMM_STAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if(recvDataBuf[0] < localDataBuf[localSize-1]){
+                MPI_Sendrecv(localDataBuf, localSize-1, MPI_FLOAT, rightNeighbor, COMM_STAGE, &recvDataBuf[1], rightSize-1, MPI_FLOAT, rightNeighbor, COMM_STAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                idxA = 0;
+                idxB = 0;
+                idxC = 0;
+                while(idxC != localSize){
+                    if(localDataBuf[idxA] < recvDataBuf[idxB]){
+                        mergeDataBuf[idxC++] = localDataBuf[idxA++];
+                        if(idxA == localSize) break;
+                    }else{
+                        mergeDataBuf[idxC++] = recvDataBuf[idxB++];
+                        if(idxB == rightSize) break;
+                    }
+                }
+
+                while(idxC != localSize){
+                    mergeDataBuf[idxC++] = localDataBuf[idxA++];
+                }
+
+                swap(localDataBuf, mergeDataBuf);
             }
         }
-        swap(localDataBuf, mergeDataBuf);
+        if(!stage && leftNeighbor != -1){
+            // get the larger half
+            // take the head and buttom to decide how many data need to exchange
+            MPI_Sendrecv(&localDataBuf[0], 1, MPI_FLOAT, leftNeighbor, COMM_STAGE, &recvDataBuf[leftSize-1], 1, MPI_FLOAT, leftNeighbor, COMM_STAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if(recvDataBuf[leftSize-1] > localDataBuf[0]){
+                MPI_Sendrecv(&localDataBuf[1], localSize-1, MPI_FLOAT, leftNeighbor, COMM_STAGE, recvDataBuf, leftSize-1, MPI_FLOAT, leftNeighbor, COMM_STAGE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);    
+                idxA = localSize - 1;
+                idxB = leftSize - 1;
+                idxC = localSize - 1;
+
+                while(idxC != -1){
+                    if(localDataBuf[idxA] > recvDataBuf[idxB]){
+                        mergeDataBuf[idxC--] = localDataBuf[idxA--];
+                        if(idxA == -1) break;
+                    }else{
+                        mergeDataBuf[idxC--] = recvDataBuf[idxB--];
+                        if(idxB == -1) break;
+                    }
+                }
+
+                while(idxC != -1){
+                    mergeDataBuf[idxC--] = localDataBuf[idxA--];
+                }
+                swap(localDataBuf, mergeDataBuf);
+            }
+        }
+        stage ^= 1;
     }
+
     // 5. Write data
     MPI_File fout;
     MPI_File_open(MPI_COMM_WORLD, argv[3], MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fout);

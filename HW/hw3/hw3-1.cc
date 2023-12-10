e2f92eb4d2732b7375de2e0e0d7cb44a93ccc5e7
109062101 <andrea.843843@gmail.com>
2023-12-10 15:14:19 +0800

    add lab4 and hw3-1

---
+++ HW/hw3/hw3-1.cc
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <pthread.h>
+#include <math.h>
+#include <iostream>
+#include <omp.h>
+#define INF ((1 << 30) - 1)
+
+using namespace std;
+int numOfThread, v, e;
+int dis[6001][6001];
+
+void read_input(const char* filename){
+    FILE* infile;
+    infile = fopen(filename, "rb");
+    fread(&v, sizeof(int), 1, infile);
+    fread(&e, sizeof(int), 1, infile);
+    for(int i = 0; i < v; i++){
+        for(int j = 0; j < v; j++){
+            if(i == j) dis[i][j] = 0;
+            else dis[i][j] = INF;
+        }
+    }
+    int edge[3];
+    for(int i = 0; i < e; i++){
+        fread(edge, sizeof(int), 3, infile);
+        dis[edge[0]][edge[1]] = edge[2];
+    }
+    fclose(infile);
+    return;
+}
+
+void write_output(const char* filename){
+    FILE* outfile = fopen(filename, "wb");
+    for(int i = 0; i < v; i++){
+        fwrite(dis[i], sizeof(int), v, outfile);
+    }
+    fclose(outfile);
+}
+
+int main(int argc, char** argv){
+    cpu_set_t cpu_set;
+    sched_getaffinity(0, sizeof(cpu_set), &cpu_set);
+    numOfThread = CPU_COUNT(&cpu_set);
+    read_input(argv[1]);
+    // for(int i = 0; i < v; i++){
+    //     for(int j = 0; j < v; j++){
+    //         std::cout << dis[i][j] << " ";
+    //     }
+    //     std::cout << "\n";
+    // }
+    for(int k = 0; k < v; k++){
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < v; j++){
+                dis[i][j] = min(dis[i][j], dis[i][k]+dis[k][j]);
+            }
+        }
+    }
+
+    write_output(argv[2]);
+    return 0;
+}
